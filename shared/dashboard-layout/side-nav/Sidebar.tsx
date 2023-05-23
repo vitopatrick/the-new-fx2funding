f460852610aa97646e87a4cb140d2c8f64af6693
theWarlock <fecypher@gmail.com>
2023-05-23 22:20:31 +0100

    added logo

--- shared/dashboard-layout/side-nav/Sidebar.tsx
+++ shared/dashboard-layout/side-nav/Sidebar.tsx
@@ -26,6 +26,9 @@ const Sidebar = () => {
         {/* first links flex container */}
         <div>
           {/* logo flex container */}
+          <div className="mb-8 font-bold text-button uppercase font-main text-xl">
+            <img src="/logo.webp" alt="logo" className="w-[50%] p-2" />
+          </div>
           {FIRST_LINKS.map((link) => (
             <Link
               href={link.path}
