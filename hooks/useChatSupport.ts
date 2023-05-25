0ae7c81270984ce4a40d0fc589089b15a6b6d58d
theWarlock <fecypher@gmail.com>
2023-05-25 22:32:06 +0100

    changed chat support

--- hooks/useChatSupport.ts
+++ hooks/useChatSupport.ts
@@ -2,10 +2,16 @@ import { useEffect } from "react";
 
 export const useChatSupport = () => {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "//code.jivosite.com/widget/WXViXlVVth";
-
-    document.body.appendChild(script);
+    var Tawk_API: any = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function () {
+      var s1: any = document.createElement("script"),
+        s0: any = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = "https://embed.tawk.to/646fd38aad80445890ef26c1/1h1ad4m4s";
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
   }, []);
 };
