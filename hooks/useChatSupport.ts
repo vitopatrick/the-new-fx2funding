12a1752ec9ec763362adccbcf064cc20563e7022
theWarlock <fecypher@gmail.com>
2023-06-14 10:59:24 +0100

    changed the tawk.to

--- hooks/useChatSupport.ts
+++ hooks/useChatSupport.ts
@@ -8,7 +8,7 @@ export const useChatSupport = () => {
       var s1: any = document.createElement("script"),
         s0: any = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/646fd38aad80445890ef26c1/1h1ad4m4s";
+      s1.src = "https://embed.tawk.to/64898d9bcc26a871b0227549/default";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
