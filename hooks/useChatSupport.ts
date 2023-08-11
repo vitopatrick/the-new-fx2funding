7c8e7c581152908edf8d20967c60ee1639a3c420
theWarlock <patrickvito2012@gmail.com>
2023-08-12 00:09:28 +0100

    changed chat support

--- hooks/useChatSupport.ts
+++ hooks/useChatSupport.ts
@@ -8,10 +8,11 @@ export const useChatSupport = () => {
       var s1: any = document.createElement("script"),
         s0: any = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/64898d9bcc26a871b0227549/default";
+      s1.src = "https://embed.tawk.to/64d6bf6f94cf5d49dc69e508/1h7jdnjma";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
     })();
   }, []);
 };
+
