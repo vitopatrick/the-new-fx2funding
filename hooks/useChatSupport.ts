daf8f76bdd0a7ff6ee487112fd0c98a6d3558683
theWarlock <patrickvito2012@gmail.com>
2023-08-15 16:50:30 +0100

    changed the address'

--- hooks/useChatSupport.ts
+++ hooks/useChatSupport.ts
@@ -8,11 +8,10 @@ export const useChatSupport = () => {
       var s1: any = document.createElement("script"),
         s0: any = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = "https://embed.tawk.to/64d6bf6f94cf5d49dc69e508/1h7jdnjma";
+      s1.src = "https://embed.tawk.to/64db9e4e94cf5d49dc6a8dc4/1h7su4q61";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
       s0.parentNode.insertBefore(s1, s0);
     })();
   }, []);
 };
-
